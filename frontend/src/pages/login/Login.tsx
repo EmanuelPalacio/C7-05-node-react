d48beab3b72b2a293fb4665f8ea494e9f5207c97
Isaac Celi <sebasceliamores@hotmail.com>
2022-09-27 17:42:05 -0500

    Maquetación del formulario - Inicio de sesión

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,7 +1,28 @@
+import ImageLoginSvg from '@/components/svg/ImageLogin';
+import LogoSvg from '@/components/svg/Logo';
 import React from 'react';
+import styles from './styles/login.module.css';
 
 const Login: React.FC = () => {
-  return <div>Login</div>;
+  return (
+    <div className={styles.loginContainer}>
+      <div className={styles.imgContainer}>
+        {' '}
+        <ImageLoginSvg />
+      </div>
+      <form className={styles.formContainer}>
+        <span className={styles.formContainer_logo}>
+          <LogoSvg svgProp={{ width: 166, height: 180 }} />
+        </span>
+        <input className={styles.formContainer_input} type='text' name='username' placeholder='usuario' />
+        <input className={styles.formContainer_input} type='text' name='password' placeholder='contraseña' />
+        <button className={styles.formContainer_button}>Iniciar sesión</button>
+        <a>¿Olvidaste tu contraseña?</a>
+      </form>
+    </div>
+  );
 };
+
+<style></style>;
 
 export default Login;
