92f65b395c47f4755953c0c7f1511be0b6ae8545
Maximiliano Miño <libertadores323@gmail.com>
2022-10-04 23:47:59 -0300

    Creación, actualización y eliminación de turnos

--- backend/src/controllers/index.controller.js
+++ backend/src/controllers/index.controller.js
@@ -1,3 +1,4 @@
 module.exports = {
   auth: require('./auth/authController.controller'),
+  turn: require('./turn/turnController.controller'),
 };
--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -1,13 +1,15 @@
-const { auth } = require('../controllers/index.controller');
-
-
+const { auth, turn } = require('../controllers/index.controller');
 
 module.exports = (router) => {
   router.post('/auth/register', auth.createUser);
   router.post('/auth/login', auth.login);
   router.get('/auth/', auth.isAuth);
-  
-  
+
+  router.post('/turns', turn.createTurn);
+  router.put('/turns/:id', turn.updateTurn);
+  router.get('/turns/', turn.getTurns);
+  router.get('/turns/:id', turn.getTurn);
+  router.delete('/turns/:id', turn.deleteTurn);
 
   return router;
 };
