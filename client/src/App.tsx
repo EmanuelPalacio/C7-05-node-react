d7b1b2e4b86ae65695726e08d6a2752267b0f78f
Emanuel Palacio <emanuelpalacio.dev@gmail.com>
2023-07-26 16:09:53 -0300

    config

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<h1>hola</h1>} />
+    </Routes>
+  );
+}
+
+export default App;
