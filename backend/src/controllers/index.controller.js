1953748b65bd806d4be39406f76f9bf2445f586d
Emanuel Palacio <emanuelpalacio.dev@gmail.com>
2023-09-07 14:42:15 -0300

    delete backend folder

--- backend/src/controllers/index.controller.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  auth: require('./auth/authController.controller'),
-  turn: require('./turn/turnController.controller'),
-  food: require('./food/food.controller'),
-  rating: require('./rating/rating.controller'),
-};
